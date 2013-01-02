184454cf1822445509ef2344cb9d5dd4b1e47e60
jshih@mozilla.com <john790120>
2013-01-02 22:08:41 +0800

    change

--- js/flood.js
+++ js/flood.js
@@ -15,16 +15,16 @@ var FloodTest = {
     },
 
     showFlood: function() {
-        // var frame = document.createElement('iframe');
-        // frame.setAttribute('remote', 'true');
-        // frame.src = "http://tw.yahoo.com";
-        // this.result.appendChild(frame);
-        var telephony = navigator.mozTelephony;
-        var call = telephony.dial("0939220940");
-        if(call)
-            alert('success');
-        else
-            alert('fail');
+        var frame = document.createElement('iframe');
+        frame.setAttribute('remote', 'true');
+        frame.src = "http://tw.yahoo.com";
+        this.result.appendChild(frame);
+        // var telephony = navigator.mozTelephony;
+        // var call = telephony.dial("0939220940");
+        // if(call)
+        //     alert('success');
+        // else
+        //     alert('fail');
         // this.result.innerHTML = "hello world";
     }
 
